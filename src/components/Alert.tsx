1ab9b120985f78819f1239fa5fb9a149e0d463ee
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2023-12-11 00:34:29 +0100

    fix(Alert): constraining children

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -8,10 +8,10 @@ export function Alert({ children }: AlertProps) {
   return (
     <div
       role="alert"
-      className="grid grid-cols-[auto_1fr] gap-x-2 rounded-2xl border border-ui-neutral-2 bg-ui-neutral-1 p-4 text-base text-ui-neutral-5"
+      className="flex gap-x-2 rounded-2xl border border-ui-neutral-2 bg-ui-neutral-1 p-4 text-base text-ui-neutral-5"
     >
-      <InformationCircleIcon className="h-6 w-6" />
-      {children}
+      <InformationCircleIcon className="h-6 w-6 flex-none" />
+      <div>{children}</div>
     </div>
   );
 }
