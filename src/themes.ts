ac2f8677fe0be52019ced80d076ae6d94143d2b1
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2023-12-11 21:48:00 +0100

    refactor(themes): reorder keys

--- src/themes.ts
+++ src/themes.ts
@@ -4,7 +4,7 @@ function oklchCoords(color: string) {
   return color.replace(/^oklch\((.+)\)$/u, "$1");
 }
 
-type ColorGroup = "neutral" | "primary";
+type ColorGroup = "primary" | "neutral";
 type ColorLevel = 0 | 1 | 2 | 3 | 4 | 5;
 
 type ColorFamily<T extends ColorGroup> = {
@@ -21,33 +21,33 @@ const colorL4 = { light: 700, dark: 300 } as const;
 const colorL5 = { light: 800, dark: 200 } as const;
 
 export const lightTheme = {
-  "--color-neutral-0": oklchCoords(colors.zinc[colorL0.light]),
-  "--color-neutral-1": oklchCoords(colors.zinc[colorL1.light]),
-  "--color-neutral-2": oklchCoords(colors.zinc[colorL2.light]),
-  "--color-neutral-3": oklchCoords(colors.zinc[colorL3.light]),
-  "--color-neutral-4": oklchCoords(colors.zinc[colorL4.light]),
-  "--color-neutral-5": oklchCoords(colors.zinc[colorL5.light]),
-
   "--color-primary-0": oklchCoords(colors.blue[colorL0.light]),
   "--color-primary-1": oklchCoords(colors.blue[colorL1.light]),
   "--color-primary-2": oklchCoords(colors.blue[colorL2.light]),
   "--color-primary-3": oklchCoords(colors.blue[colorL3.light]),
   "--color-primary-4": oklchCoords(colors.blue[colorL4.light]),
   "--color-primary-5": oklchCoords(colors.blue[colorL5.light]),
+
+  "--color-neutral-0": oklchCoords(colors.zinc[colorL0.light]),
+  "--color-neutral-1": oklchCoords(colors.zinc[colorL1.light]),
+  "--color-neutral-2": oklchCoords(colors.zinc[colorL2.light]),
+  "--color-neutral-3": oklchCoords(colors.zinc[colorL3.light]),
+  "--color-neutral-4": oklchCoords(colors.zinc[colorL4.light]),
+  "--color-neutral-5": oklchCoords(colors.zinc[colorL5.light]),
 } as const satisfies Theme;
 
 export const darkTheme = {
-  "--color-neutral-0": oklchCoords(colors.zinc[colorL0.dark]),
-  "--color-neutral-1": oklchCoords(colors.zinc[colorL1.dark]),
-  "--color-neutral-2": oklchCoords(colors.zinc[colorL2.dark]),
-  "--color-neutral-3": oklchCoords(colors.zinc[colorL3.dark]),
-  "--color-neutral-4": oklchCoords(colors.zinc[colorL4.dark]),
-  "--color-neutral-5": oklchCoords(colors.zinc[colorL5.dark]),
-
   "--color-primary-0": oklchCoords(colors.blue[colorL0.dark]),
   "--color-primary-1": oklchCoords(colors.blue[colorL1.dark]),
   "--color-primary-2": oklchCoords(colors.blue[colorL2.dark]),
   "--color-primary-3": oklchCoords(colors.blue[colorL3.dark]),
   "--color-primary-4": oklchCoords(colors.blue[colorL4.dark]),
   "--color-primary-5": oklchCoords(colors.blue[colorL5.dark]),
+
+  "--color-neutral-0": oklchCoords(colors.zinc[colorL0.dark]),
+  "--color-neutral-1": oklchCoords(colors.zinc[colorL1.dark]),
+  "--color-neutral-2": oklchCoords(colors.zinc[colorL2.dark]),
+  "--color-neutral-3": oklchCoords(colors.zinc[colorL3.dark]),
+  "--color-neutral-4": oklchCoords(colors.zinc[colorL4.dark]),
+  "--color-neutral-5": oklchCoords(colors.zinc[colorL5.dark]),
 } as const satisfies Theme;
