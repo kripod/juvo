ed8667e8ebeafd2e23ee15a127222c5edd60c574
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2023-12-15 23:10:40 +0100

    docs: add sample page for experimentation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 import tailwindPreset from "./src/tailwind-preset";
 
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
   presets: [tailwindPreset],
   darkMode: ["class", ".theme-dark"],
 } satisfies Config;
